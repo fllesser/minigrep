799b0db6d9da806d878313a4d2bd3427852f013e
fllesser <fllessive@gmail.com>
2025-02-10 22:07:54 +0800

    ✨feat: 改进错误处理，使用 eprintln 输出错误信息

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use minigrep::config::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|e| {
-        println!("Problem parsing arguments, {}", e);
+        eprintln!("Problem parsing arguments, {}", e);
         std::process::exit(1);
     });
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         std::process::exit(1);
     }
 }
